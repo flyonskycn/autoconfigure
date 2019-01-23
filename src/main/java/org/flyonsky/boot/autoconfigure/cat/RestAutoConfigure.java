4eb257530fee0c0faae39300acbf533ef88aace6
luowengang <2628387671@qq.com>
2019-01-23 17:43:32 +0800

    修改cat初始化

--- src/main/java/org/flyonsky/boot/autoconfigure/cat/RestAutoConfigure.java
+++ src/main/java/org/flyonsky/boot/autoconfigure/cat/RestAutoConfigure.java
@@ -1,49 +1,38 @@
 package org.flyonsky.boot.autoconfigure.cat;
 
-import java.io.IOException;
-import java.lang.invoke.MethodHandles;
 import java.util.Collections;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.ClientHttpResponse;
-import org.springframework.web.client.DefaultResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @ConditionalOnBean({RestTemplate.class})
 public class RestAutoConfigure {
-	private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
-	@Autowired
-	public void createRestTemplate(RestTemplate restTemplate) {
-		// 设置Cat 上下文拦截器
-		restTemplate.setInterceptors(Collections.singletonList(new CatRestInterceptor()));
+	@Bean
+	public CatRestConfig catRestTemplate(RestTemplate restTemplate) {
+		CatRestConfig config = new CatRestConfig(restTemplate);
+		// 初始化Cat
+		config.initCat();
+		return config;
+	}
+	
+	protected static class CatRestConfig{
 		
-		restTemplate.setErrorHandler(new DefaultResponseErrorHandler() {
-			@Override 
-			public boolean hasError(ClientHttpResponse response)
-					throws IOException {
-				try {
-					return super.hasError(response);
-				} catch (Exception e) {
-					return true;
-				}
-			}
-
-			@Override 
-			public void handleError(ClientHttpResponse response)
-					throws IOException {
-				try {
-					super.handleError(response);
-				} catch (Exception e) {
-					log.error("Exception [" + e.getMessage() + "] occurred while trying to send the request", e);
-					throw e;
-				}
-			}
-		});
+		private RestTemplate restTemplate;
+		
+		CatRestConfig(RestTemplate restTemplate){
+			this.restTemplate = restTemplate;
+		}
+		
+		/**
+		 * 初始化Cat
+		 */
+		public void initCat() {
+			// 设置Cat 上下文拦截器
+			restTemplate.setInterceptors(Collections.singletonList(new CatRestInterceptor()));
+		}
 	}
 }
